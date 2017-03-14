6a818a53fab68f5b70f51c7e2bd1726282950ad8
anthonymai14 <amai005@ucr.edu>
2017-03-14 07:13:25 +0000

    add keyboard test

---
+++ module_test/keyboard.c
@@ -0,0 +1,7 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include "/Users/antho_000/Documents/Atmel Studio/7.0/header files/timer.h"
+#include "/Users/antho_000/Documents/Atmel Studio/7.0/header files/io.c"
+#include "/Users/antho_000/Documents/Atmel Studio/7.0/header files/bit.h"
+#include "/Users/antho_000/Documents/Atmel Studio/7.0/header files/keypad.h"
